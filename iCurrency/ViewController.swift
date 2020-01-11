f94748d9648ebaea58e5a2824fd1287b997a8600
Osama Gamal <me@i0sa.com>
2020-01-11 09:57:21 +0200

    Initial Commit

---
+++ iCurrency/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iCurrency
+//
+//  Created by Osama on 1/11/20.
+//  Copyright © 2020 Osama Gamal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
