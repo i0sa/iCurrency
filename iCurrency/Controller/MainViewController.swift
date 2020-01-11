9fb19a3d6b8bce2c7f4466b1ddc03da3155588a6
Osama Gamal <me@i0sa.com>
2020-01-11 10:09:56 +0200

    Start! Prepare/Remove storyboard

--- iCurrency/Controller/MainViewController.swift
+++ iCurrency/Controller/MainViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("test")
         // Do any additional setup after loading the view.
     }
 
